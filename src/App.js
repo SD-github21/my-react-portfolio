ed001a952867ae55e8d3ce95361ca6b9e6e0a92f
SD-github21 <sapandonde.gmail.com>
2022-07-30 18:16:44 -0500

    added code for footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main>
         <About></About>
       </main>
+      <Footer></Footer>
     </div>
   );
 }
