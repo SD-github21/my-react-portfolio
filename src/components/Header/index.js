b78fbe59655107a2b1fa794788888b40a3941b14
SD-github21 <sapandonde.gmail.com>
2022-07-30 18:06:42 -0500

    added new favicon + css styling + Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../Navigation'
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <a href="/">
+                    Sapana D. Donde
+                </a>
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
