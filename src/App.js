9b8e719cdb1d5436fb715cde2e024b0801321253
SD-github21 <sapandonde.gmail.com>
2022-07-30 18:59:22 -0500

    added background image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import heroImage from './assets/images/hero-bg.jpg';
 import About from './components/About';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div> 
+    <div style={{ backgroundImage: `url(${heroImage})`}}> 
       <Header></Header>
       <main>
-        <About></About>
+        <About ></About>
       </main>
       <Footer></Footer>
     </div>
