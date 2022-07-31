a1d9d1fd91b7a85959ef5bbd3780207c5c310046
SD-github21 <sapandonde.gmail.com>
2022-07-30 22:25:03 -0500

    added coding to map over projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,62 +1,15 @@
 import React from 'react';
-import meetFriendsPicture from '../../assets/images/photo-1533174072545-7a4b6ad7a6c3.avif';
-import bellyButtonPicture from '../../assets/images/bellybutton.png';
+import Project from '../Project';
 
 function Portfolio() {
     return (
-        <section id='portfolio' class='portfolio'>
+        <section id='portfolio' className='portfolio'>
             <h1>Portfolio</h1>
-
-            <div class="samples">
-            <div class="sample shrink">
-                <h3>Olympic Medal Predictions</h3>
-                <a href="https://olympic-medal-predictions.herokuapp.com/index" target="_blank" rel="noreferrer">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Olympic_rings_without_rims.svg" alt="Olympic rings"/>
-                    <h4>                
-                        <a href="https://github.com/cdeanatx/Olympic_Medal_Predictions" target="_blank" rel="noreferrer">
-                        Link to GitHub Repo
-                        </a>
-                    </h4>
-                </a>              
+            <div >
+                <Project></Project>
             </div>
 
-            <div class="sample shrink">
-                <h3>Meet Friends</h3>
-                <a href="https://meet-friends-bootcamp.herokuapp.com/" target="_blank" rel="noreferrer">
-                    <img src={meetFriendsPicture} alt="people at a concert"/>
-                    <h4>                
-                        <a href="https://github.com/SD-github21/meet-friends" target="_blank" rel="noreferrer">
-                        Link to GitHub Repo
-                        </a>
-                    </h4>
-                </a>              
-            </div>
-
-            <div class="sample shrink">
-                <h3>Happy Hour</h3>
-                <a href="https://sd-github21.github.io/happy-hour/" target="_blank" rel="noreferrer">
-                    <img src="https://i.insider.com/5c3e2df8c3100e0710201ae3?width=1200&format=jpeg&auto=webp" alt="Bar with background of shelves with multi-colored containers of liquor"/>
-                    <h4>                
-                        <a href="https://github.com/SD-github21/happy-hour" target="_blank" rel="noreferrer">
-                        Link to GitHub Repo
-                        </a>
-                    </h4>
-                </a>              
-            </div>
-
-            <div class="sample shrink">
-                <h3>Bellybutton Biodiversity</h3>
-                <a href="https://sd-github21.github.io/Bellybutton-Biodiversity/" target="_blank" rel="noreferrer">
-                    <img src={bellyButtonPicture} alt="test tubes with dropper"/>
-                    <h4>                
-                        <a href="https://github.com/SD-github21/Bellybutton-Biodiversity" target="_blank" rel="noreferrer">
-                        Link to GitHub Repo
-                        </a>
-                    </h4>
-                </a>              
-            </div>
-
-        </div>
+        
         </section>
     )
 }
---
+++ src/components/Project/index.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import meetFriendsPicture from '../../assets/images/photo-1533174072545-7a4b6ad7a6c3.avif';
+import bellyButtonPicture from '../../assets/images/bellybutton.png';
+
+function Project() {
+
+    const [projects] = useState([
+        {
+            title: 'Olympic Medal Predictions',
+            application: 'https://olympic-medal-predictions.herokuapp.com/index',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Olympic_rings_without_rims.svg',
+            alt: 'Olympic rings',
+            github: 'https://github.com/cdeanatx/Olympic_Medal_Predictions' 
+        },
+        {
+            title: 'Meet Friends',
+            application: 'https://meet-friends-bootcamp.herokuapp.com/',
+            image: meetFriendsPicture,
+            alt: 'People at a concert',
+            github: 'https://github.com/SD-github21/meet-friends' 
+        },
+        {
+            title: 'Happy Hour',
+            application: 'https://sd-github21.github.io/happy-hour/',
+            image: 'https://i.insider.com/5c3e2df8c3100e0710201ae3?width=1200&format=jpeg&auto=webp',
+            alt: 'Bar with background of shelves with multi-colored containers of liquor',
+            github: 'https://github.com/SD-github21/happy-hour' 
+        },
+        {
+            title: 'Bellybutton Biodiversity',
+            application: 'https://sd-github21.github.io/Bellybutton-Biodiversity/',
+            image: bellyButtonPicture,
+            alt: 'Test tubes with dropper',
+            github: 'https://github.com/SD-github21/Bellybutton-Biodiversity' 
+        },
+    ])
+
+    return (
+
+    <div className="samples">
+        {projects.map((project) => (
+            <>
+            <div className="sample shrink">
+            <h3>{project.title}</h3>
+                <a href={project.application} target="_blank" rel="noreferrer">
+                    <img src={project.image} alt={project.alt}/>
+                        <h4>                
+                        <a href={project.github} target="_blank" rel="noreferrer">
+                            Link to GitHub Repo
+                        </a>
+                        </h4>
+                </a>
+            </div>
+            </> 
+        ))}             
+    </div>
+    )
+}
+
+export default Project;
