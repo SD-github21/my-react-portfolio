17f8627725e55cc25cd8253854a6b6e11ce330f4
SD-github21 <sapandonde.gmail.com>
2022-07-30 20:35:02 -0500

    added starter code to Contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <section id='contact' class='contact'>
+            <h2>Contact Form</h2>
+        </section>
+    )
+}
+
+export default Contact;
