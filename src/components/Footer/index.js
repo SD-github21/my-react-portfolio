3d0e8caf224d456336c27c48c4a97dd0b936201d
SD-github21 <sapandonde.gmail.com>
2022-08-06 01:12:32 -0500

    added icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import github from '../../assets/images/github.png'
+import stackOverflow from '../../assets/images/stackoverflow.png.ico';
 
 function Footer() {
     return (
         <footer>
-        <h2>Thank you for visiting my portfolio!</h2> 
+        <h2>Thank you for visiting my portfolio!</h2>
+        <a href="https://github.com/SD-github21" target="_blank" rel="noreferrer">
+            <img src={github} alt="github icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/sapana-donde" target="_blank" rel="noreferrer">
+            <img src="https://icons.iconarchive.com/icons/graphics-vibe/simple-rounded-social/64/linkedin-icon.png" alt="linkedIn icon" />
+        </a>
+        <a href="https://stackoverflow.com/users/16525678/dreamstar-sc" target="_blank" rel="noreferrer">
+            <img src={stackOverflow} alt="linkedIn icon" />
+        </a>
+
+        
       </footer>  
     )
 }
